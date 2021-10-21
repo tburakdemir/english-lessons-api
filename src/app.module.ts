9f0460142f16983eed30fddcaa664303f92859c4
Burak Demir <tburakdemir@gmail.com>
2021-10-21 23:21:04 +0300

    Get uri from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LessonsModule } from './lessons/lessons.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     LessonsModule,
-    MongooseModule.forRoot('mongodb://localhost/engvid'),
+    MongooseModule.forRoot(process.env.DATABASE_CONN_URI),
   ],
   controllers: [AppController],
   providers: [AppService],
